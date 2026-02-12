9b2248be93facfdab1de57fa65dc48bf1e911d05
Ismail <ghvh8176@gmail.com>
2026-02-12 16:26:38 -0600

    create serializer

--- 1. todo_app_api/tasks/models.py
+++ 1. todo_app_api/tasks/models.py
@@ -1,13 +1,13 @@
 '''Here we can connect to database'''
 
-from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models  #imports models
+from django.core.validators import MinValueValidator, MaxValueValidator #imports validators
 
 class Task(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
-    completed = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255) # title in the database with max 255 chars
+    description = models.TextField(blank=True) # description will be created in db
+    completed = models.BooleanField(default=False) # will be default incomplete
+    created_at = models.DateTimeField(auto_now_add=True) # gives dates created not updated
     quantity = models.IntegerField(
         default=1,
         validators=[
---
+++ 1. todo_app_api/tasks/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers  # imports serializer
+from .models import Task # imports from model
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'quantity', 'completed', 'created_at']
+        read_only_fields = ['id', 'created_at']
