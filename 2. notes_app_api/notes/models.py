6e18580d838b92968d5ed0b09bd7657906798133
Ismail <ghvh8176@gmail.com>
2026-02-12 17:52:55 -0600

    make migrations

--- 2. notes_app_api/notes/models.py
+++ 2. notes_app_api/notes/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Notes(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
