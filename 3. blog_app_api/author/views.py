e43acc0f98fd9c25a6b5c52c8f223cf22efbb104
Ismail <ghvh8176@gmail.com>
2026-02-13 16:06:23 -0600

    add to views blog

--- 3. blog_app_api/author/views.py
+++ 3. blog_app_api/author/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets, views
-from .models import AuthorModel
-from .serializers import AuthorSerializer
+from .models import AuthorModel, BlogPostModel
+from .serializers import AuthorSerializer, BlogPostSerializer
 from rest_framework.permissions import AllowAny
 
 class AuthorViews(viewsets.ModelViewSet):
     serializer_class = AuthorSerializer
     queryset = AuthorModel.objects.all()
+    permission_classes = [AllowAny]
+
+class BlogPostModel(viewsets.ModelViewSet):
+    serializer_class = BlogPostSerializer
+    queryset = BlogPostModel.objects.all()
     permission_classes = [AllowAny]
