009ed094149b3645ec14fa6dd8133d6546e7886f
Ismail <ghvh8176@gmail.com>
2026-02-13 11:38:00 -0600

    make author urls

--- 3. blog_app_api/author/urls.py
+++ 3. blog_app_api/author/urls.py
@@ -1,3 +1,11 @@
 from django.urls import path, include
 from .models import AuthorModel
 from rest_framework.routers import DefaultRouter
+from .views import AuthorViews
+
+router = DefaultRouter()
+router.register('author', AuthorViews)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
