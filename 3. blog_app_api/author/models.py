2c0840bfc729c843605c8f467ab62d971eb2fef9
Ismail <ghvh8176@gmail.com>
2026-02-13 12:19:59 -0600

    make migrations for blogs

--- 3. blog_app_api/author/models.py
+++ 3. blog_app_api/author/models.py
@@ -8,3 +8,13 @@ class AuthorModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.name
+
+class BlogPostModel(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(max_length=255)
+    author = models.ForeignKey(AuthorModel, on_delete=models.CASCADE) # foreing key
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
+    
