846b9860b99989101583f4bac81e5238d80176fd
Ismail <ghvh8176@gmail.com>
2026-02-13 18:01:37 -0600

    add to urls

--- 2. notes_app_api/notes/urls.py
+++ 2. notes_app_api/notes/urls.py
@@ -1,9 +1,10 @@
-from .views import NotesViewSet
+from .views import NotesViewSet, CategoryViewSet
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('notes', NotesViewSet)
+router.register('categories', CategoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
