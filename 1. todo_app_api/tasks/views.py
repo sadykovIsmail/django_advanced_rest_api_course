016a0f4f287fa73bf6857120d3bdd8ef3239c751
Ismail <ghvh8176@gmail.com>
2026-02-14 13:42:39 -0600

    update viewSet

--- 1. todo_app_api/tasks/views.py
+++ 1. todo_app_api/tasks/views.py
@@ -1,10 +1,14 @@
 from rest_framework import viewsets # gives all CRUD operations
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 
 class TaskViewSet(viewsets.ModelViewSet):
     """Api endpoint for tasks."""
     serializer_class = TaskSerializer
     queryset = Task.objects.all() # which tasks are available
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
