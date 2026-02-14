e18da6f63803e380a6460b1c91402e1c818a263d
Ismail <ghvh8176@gmail.com>
2026-02-14 09:07:43 -0600

    add to the views

--- 2. notes_app_api/notes/views.py
+++ 2. notes_app_api/notes/views.py
@@ -1,7 +1,7 @@
-from .serializers import NotesSerializer, CategorySerializer
+from .serializers import NotesSerializer, CategorySerializer, TagSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
-from .models import Note, Category
+from .models import Note, Category, Tag
 
 class CategoryViewSet(viewsets.ModelViewSet):
     """End points of category"""
@@ -13,4 +13,10 @@ class NotesViewSet(viewsets.ModelViewSet):
     """End points of API"""
     serializer_class = NotesSerializer
     queryset = Note.objects.all()
+    permission_classes = [AllowAny]
+
+class TagsViewSet(viewsets.ModelViewSet):
+    """End point of API"""
+    serializer_class = TagSerializer
+    queryset = Tag.objects.all()
     permission_classes = [AllowAny]
