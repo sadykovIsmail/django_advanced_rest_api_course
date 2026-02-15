34611fa517ae60370fa09fa3c5900cba25b39af8
Ismail <ghvh8176@gmail.com>
2026-02-15 10:10:17 -0600

    add swagger to urls

--- 2. notes_app_api/core/urls.py
+++ 2. notes_app_api/core/urls.py
@@ -20,10 +20,13 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView
 )
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('notes.urls')),
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/refresh', TokenRefreshView.as_view(), name='token_refresh')
+    path('api/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/schema', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs', SpectacularSwaggerView.as_view('url_name = schema'))
 ]
