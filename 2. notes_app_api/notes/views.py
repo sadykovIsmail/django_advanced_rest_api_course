41965b1677cac8a122ed9ae49b6bc2c2c003e19f
Ismail <ghvh8176@gmail.com>
2026-02-15 10:40:58 -0600

    clean views bug

--- 2. notes_app_api/notes/views.py
+++ 2. notes_app_api/notes/views.py
@@ -1,8 +1,6 @@
 from .serializers import NotesSerializer, CategorySerializer, TagSerializer
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
 from .models import Note, Category, Tag
-from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 
 class CategoryViewSet(viewsets.ModelViewSet):
     """End points of category"""
@@ -13,8 +11,6 @@ class NotesViewSet(viewsets.ModelViewSet):
     """End points of API"""
     serializer_class = NotesSerializer
     queryset = Note.objects.all()
-    authentication_classes = [TokenAuthentication, SessionAuthentication]
-    permission_classes = [IsAuthenticated]
     def get_queryset(self):
         return Note.objects.filter(user=self.request.user)
 
