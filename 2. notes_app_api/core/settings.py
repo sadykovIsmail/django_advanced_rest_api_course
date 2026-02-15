ad6ba7b576c1da350dd1c52d73ad07028ba3d608
Ismail <ghvh8176@gmail.com>
2026-02-15 09:45:30 -0600

    add postgres to settings

--- 2. notes_app_api/core/settings.py
+++ 2. notes_app_api/core/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'drf_project',
+        'USER': 'postgres',
+        'PASSWORD': 'POSTGRES',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
