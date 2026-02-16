53ac186f5088d28b7614c6fb1d3fb257e01fdea1
Ismail <ghvh8176@gmail.com>
2026-02-15 18:56:45 -0600

    test in the browser

--- 3. blog_app_api/Dockerfile
+++ 3. blog_app_api/Dockerfile
@@ -1,28 +1,15 @@
-FROM python:3.12-alpine
+FROM python:3.12
 
-LABEL maintainer="blog-app"
-ENV PYTHONUNBUFFERED=1
-
+# Set working directory
 WORKDIR /app
 
-ARG DEV=false
-
-COPY requirements.txt /tmp/requirements.txt
-COPY requirements.dev.txt /tmp/requirements.dev.txt
+# Copy everything from local 'app' folder
 COPY ./app /app
 
-RUN apk add --update --no-cache \
-        gcc libpq-dev postgresql-client jpeg-dev \
-    && python -m venv /py \
-    && /py/bin/pip install --upgrade pip \
-    && /py/bin/pip install -r /tmp/requirements.txt \
-    && if [$DEV = "true" ]; then /py/bin/pip install -r /tmp/requirements.dev.txt; fi \
-    && rm -rf /tmp \
-    && apk del gcc libpq-dev
+# Copy requirements separately for caching
+COPY requirements.txt /app
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
 
-ENV PATH="/py/bin:$PATH"
-
-RUN adduser --disabled-password --no-create-home django-user
-USER django-user
-
-EXPOSE 8000
+# Default command
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- 3. blog_app_api/app/core/settings.py
+++ 3. blog_app_api/app/core/settings.py
@@ -11,11 +11,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = os.getenv("DEBUG") == "1"
+SECRET_KEY = "rg(42t8*0ca^o=10ahvyh7cd530@q1_#w88cszjf8=+r(t95+d"
+DEBUG = "1"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -78,11 +81,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASS"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT")
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
