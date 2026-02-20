71952374534d1991134954cf6004877c25c2d072
Ismail <ghvh8176@gmail.com>
2026-02-20 01:04:17 -0600

    update views to upload umages

--- 3. blog_app_api/app/author/views.py
+++ 3. blog_app_api/app/author/views.py
@@ -1,8 +1,11 @@
-from rest_framework import viewsets, views
+from rest_framework import viewsets, views, status
 from .models import AuthorModel, BlogPostModel
-from .serializers import AuthorSerializer, BlogPostSerializer
+from .serializers import AuthorSerializer, BlogPostSerializer, PostImageSerializer
 from rest_framework.permissions import IsAuthenticated, BasePermission
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
 
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
@@ -27,3 +30,20 @@ class BlogPostViews(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         # Automatically assign the logged-in user
         serializer.save(user=self.request.user)
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+        parser_classes=[MultiPartParser, FormParser],
+    )
+    def upload_image(self, request, pk=None):
+        post = self.get_object()
+        serializer = self.get_serializer(post, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
