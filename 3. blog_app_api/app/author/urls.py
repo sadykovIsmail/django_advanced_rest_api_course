eccf6e2b7bcfa499648c27ab7f386d8932915c8b
Ismail <ghvh8176@gmail.com>
2026-02-25 16:04:41 -0600

    fix typo bug

--- 3. blog_app_api/app/author/urls.py
+++ 3. blog_app_api/app/author/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 from .models import AuthorModel
 from rest_framework.routers import DefaultRouter
 from .views import AuthorViews, BlogPostViews
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 router = DefaultRouter()
 router.register('author', AuthorViews)
@@ -9,4 +12,4 @@ router.register('posts', BlogPostViews)
 
 urlpatterns = [
     path('', include(router.urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- 3. blog_app_api/app/author/views.py
+++ 3. blog_app_api/app/author/views.py
@@ -50,5 +50,5 @@ class BlogPostViews(viewsets.ModelViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
 
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
