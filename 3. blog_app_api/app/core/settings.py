84e8a4ade25f6a995dbd845670064d368cbdb03f
Ismail <ghvh8176@gmail.com>
2026-02-25 14:22:20 -0600

    update

--- 3. blog_app_api/app/core/settings.py
+++ 3. blog_app_api/app/core/settings.py
@@ -91,7 +91,9 @@ DATABASES = {
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
