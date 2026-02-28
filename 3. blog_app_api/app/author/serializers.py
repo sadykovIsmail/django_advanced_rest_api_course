f1051dc88a68afe28f0b4e09862a481e99c0fca4
Ismail <ghvh8176@gmail.com>
2026-02-28 12:42:09 -0600

    fix bug

--- 3. blog_app_api/app/author/serializers.py
+++ 3. blog_app_api/app/author/serializers.py
@@ -10,8 +10,7 @@ class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = AuthorModel
         fields = ['id', 'name', 'email', 'created_at', 'user', 'user_name']
-        read_only_fields = ['id', 'created_at', 'user', 'user_name']
-
+        read_only_fields = ['id', 'created_at', 'user']
 
 
 
@@ -23,8 +22,8 @@ class BlogPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogPostModel
         fields = "__all__"
-        read_only_fields = ['id', 'created_at', 'updated_at', 'author_name', 'user']
-
+        read_only_fields = ['id', 'created_at', 'updated_at', 'user']
+        
 class PostImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogPostModel
