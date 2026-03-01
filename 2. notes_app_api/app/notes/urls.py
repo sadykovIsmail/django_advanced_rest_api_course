f4c95e8b861bd49959b8007a927367ff9eee1730
Ismail <ghvh8176@gmail.com>
2026-03-01 04:03:50 -0600

    feat(notes): add media url and media root to urls (#4)

--- 2. notes_app_api/app/notes/urls.py
+++ 2. notes_app_api/app/notes/urls.py
@@ -1,6 +1,8 @@
 from .views import NotesViewSet, CategoryViewSet, TagsViewSet
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from django.conf.urls.static import static
+from django.conf import settings
 
 router = DefaultRouter()
 router.register('notes', NotesViewSet, basename='note')
@@ -8,5 +10,5 @@ router.register('categories', CategoryViewSet)
 router.register('tags', TagsViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
