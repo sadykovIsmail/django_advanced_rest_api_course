9cd87e55d226d0630262e66a5d12ea47c31c2682
Ismail <ghvh8176@gmail.com>
2026-03-05 16:10:39 -0600

    test(blog): create a setup to test post (#6)

--- 3. blog_app_api/app/author/tests/test_posts.py
+++ 3. blog_app_api/app/author/tests/test_posts.py
@@ -16,3 +16,15 @@ def create_user(usernane, password):
 def create_author(user, name="Example", email="email@example.com"):
     return AuthorModel.objects.create(user=user, name=name, email=email)
 
+
+class test_post_create(TestCase):
+    # check if user assigning automatically from token
+    def setUp(self):
+        self.client = APIClient()
+        self.user = create_user(usernane="My user", password="123example")
+        self.client.force_authenticate(user=self.user)
+        self.author = create_author(self.user, "My Author", "myauthor@example.com")
+        self.post_link = reverse("blogpostmodel-list")
+
+    
+    payload = {}
