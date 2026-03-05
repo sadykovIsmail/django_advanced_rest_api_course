7d80c008aa2814ef4d216d097696dafe8a48aecf
Ismail <ghvh8176@gmail.com>
2026-03-05 00:12:55 -0600

    test(blog): create reusable author creator for testing author (#6)

--- 3. blog_app_api/app/author/tests/test_author.py
+++ 3. blog_app_api/app/author/tests/test_author.py
@@ -7,16 +7,24 @@ from rest_framework import status
 from rest_framework.test import APIClient
 User = get_user_model()
 
+AUTHOR_LIST_LINK = reverse("authormobile-list")
 
 
 def create_user(username, password):
     # for creating reusable user
     return User.objects.create(username=username, password=password)
 
+def create_author(user, name="Example", email="example@example.com"):
+    # creatign author
+    return AuthorModel.objects.create(user=user, name=name, email=email)
+
 class test_author_create(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.user = create_user
+        self.user = create_user()
         self.client.force_authenticate(user=self.user)
+        self.author_list_link = reverse("authormodel-list")
 
-    
+
+
+
