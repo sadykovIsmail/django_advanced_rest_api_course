d619c5b84f864bcc8daaa49bdd2a08d721c03fab
Ismail <ghvh8176@gmail.com>
2026-03-04 23:45:00 -0600

    test(blog): create reusable user creator (#6)

--- 3. blog_app_api/app/author/tests/test_author.py
+++ 3. blog_app_api/app/author/tests/test_author.py
@@ -5,3 +5,8 @@ from django.urls import reverse
 
 from rest_framework import status
 from rest_framework.test import APIClient
+User = get_user_model()
+
+def create_user(username, password):
+    # for creating reusable user
+    return User.objects.create(username=username, password=password)
