76a777762e4d0efe5d6f0ef6b6dd49ef4db7b95e
Ismail <ghvh8176@gmail.com>
2026-03-08 00:08:11 -0600

    test(notes): initialize testing category endpoint to create category for notes

--- 2. notes_app_api/app/notes/tests/test_category.py
+++ 2. notes_app_api/app/notes/tests/test_category.py
@@ -1,7 +1,17 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 from rest_framework import status
 from rest_framework.test import APIClient
 
+User = get_user_model()
 
+def create_user(username="testuser", password="testpass123"):
+    return User.objects.create(username=username, password=password)
+
+class CategoryTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = create_user("User1", "User1pass23")
+        self.client.force_authenticate(user=self.user)
