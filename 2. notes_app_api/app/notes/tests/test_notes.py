f72a66a0b117604591ff80050ab58aa2f648d965
Ismail <ghvh8176@gmail.com>
2026-03-08 14:19:16 -0500

    test(notes): import all dependencies to test notes endpoint (#8)

---
+++ 2. notes_app_api/app/notes/tests/test_notes.py
@@ -0,0 +1,8 @@
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from ..models import Note, Tag, Category
